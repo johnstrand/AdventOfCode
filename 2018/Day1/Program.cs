de99a91895ffc9d3fcbe69bbcb312e774be2da13
John Strand <john.strand@dekiru.se>
2018-12-01 10:10:28 +0100

    2018.01 Parts 1 & 2

---
+++ 2018/Day1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var seen = new HashSet<int>();
+            var seed = 0;
+            var running = true;
+            var first = true;
+            while (running)
+            {
+                seed = File.ReadAllLines("Input.txt").Select(int.Parse).Aggregate(seed, (acc, cur) =>
+                {
+                    if (seen.Contains(acc) && running)
+                    {
+                        Console.WriteLine(acc);
+                        running = false;
+                    }
+                    seen.Add(acc);
+                    return acc + cur;
+                });
+                if (first)
+                {
+                    Console.WriteLine(seed);
+                    first = false;
+                }
+            }
+            Console.Read();
+        }
+    }
+}
