08b1e59f920248efeb3251026d86f980161c65e6
John Strand <john.strand@dekiru.se>
2018-12-03 19:16:18 +0100

    Implemented part 2

--- 2018/Day3/Program.cs
+++ 2018/Day3/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -12,7 +11,7 @@ namespace Day3
         static void Main(string[] args)
         {
             var fabric = new int[1000 * 1000];
-            var idIndex = new Dictionary<int, int>();
+            var idIndex = new Dictionary<int, List<int>>();
             foreach (var row in File.ReadAllLines("Input.txt"))
             {
                 var m = Regex.Match(row, @"#(?<id>\d+) @ (?<start>\d+,\d+): (?<end>\d+x\d+)");
@@ -20,31 +19,43 @@ namespace Day3
                 var start = Parse(m.Groups["start"].Value);
                 var size = Parse(m.Groups["end"].Value);
 
-                var key = start.X + start.Y * 1000;
-                if (!idIndex.ContainsKey(key))
-                    idIndex.Add(key, id);
+                idIndex.Add(id, new List<int>());
 
                 for (var y = start.Y; y < start.Y + size.Y; y++)
                 {
                     for (var x = start.X; x < start.X + size.X; x++)
                     {
                         fabric[x + y * 1000]++;
+                        idIndex[id].Add(x + y * 1000);
                     }
                 }
             }
             Console.WriteLine(fabric.Count(i => i >= 2));
 
-            var intact = fabric.Select((v, i) => (v: v, i: i)).First(x => x.v == 1);
+            var intact = idIndex.FirstOrDefault(ix => ix.Value.All(i => fabric[i] == 1));
 
-            Console.WriteLine(idIndex[intact.i]);
+            Console.WriteLine(intact.Key);
 
             Console.Read();
         }
+
         static Point Parse(string text)
         {
             var parts = text.Split(',', 'x').Select(int.Parse).ToList();
 
             return new Point(parts[0], parts[1]);
+        }
+    }
+
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
         }
     }
 }
