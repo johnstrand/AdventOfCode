7415577e092233bf4aaeb33a8b4fad9e593ea140
John Strand <john.strand@dekiru.se>
2018-12-03 19:04:21 +0100

    Began work on 2015.06

--- 2015/Day6/Program.cs
+++ 2015/Day6/Program.cs
@@ -1,36 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Day6
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Part1();
         }
 
-        static void Part1()
+        private static void Part1()
         {
+            if(Directory.Exists("output1"))
+            {
+                Directory.Delete("output1", true);
+            }
+            Directory.CreateDirectory("output1");
             var grid = new Grid(1000, 1000);
-            grid.Register("on", (state, start, end) =>
+            grid.Register("turn on", (state, start, end) =>
             {
                 For(start, end, p =>
                 {
-                    state[p.X, p.Y] = true;
+                    state[p.X, p.Y] = 1;
                 });
 
                 return state;
             });
+
+            grid.Register("turn off", (state, start, end) =>
+            {
+                For(start, end, p =>
+                {
+                    state[p.X, p.Y] = 0;
+                });
+
+                return state;
+            });
+
+            grid.Register("toggle", (state, start, end) =>
+            {
+                For(start, end, p =>
+                {
+                    state[p.X, p.Y] = (byte)(state[p.X, p.Y] == 0 ? 1 : 0);
+                });
+
+                return state;
+            });
+            var frame = 1;
+            foreach(var line in File.ReadAllLines("Input.txt"))
+            {
+                Console.Write($"Processing frame {frame.ToString("00000")}\r");
+                var m = Regex.Match(line, @"^(?<command>.+) (?<from>.+?) through (?<to>.+?)$");
+                var cmd = m.Groups["command"].Value;
+                var from = ParsePoint(m.Groups["from"].Value);
+                var to = ParsePoint(m.Groups["to"].Value);
+                grid.Raise(cmd, from, to);
+                grid.AsBitmap().Save($"output1\\frame{frame.ToString("00000")}.png", ImageFormat.Png);
+                frame++;
+            }
+            Console.WriteLine();
         }
 
-        static void For(Point start, Point end, Action<Point> action)
+        private static Point ParsePoint(string input)
         {
-            for(var y = start.Y; y <= end.Y; y++)
+            var parts = input.Split(',').Select(int.Parse).ToList();
+            return new Point(parts[0], parts[1]);
+        }
+        private static void For(Point start, Point end, Action<Point> action)
+        {
+            for (var y = start.Y; y <= end.Y; y++)
             {
-                for(var x = start.X; x <= end.X; x++)
+                for (var x = start.X; x <= end.X; x++)
                 {
                     action(new Point(x, y));
                 }
@@ -38,20 +84,20 @@ namespace Day6
         }
     }
 
-    class Grid
+    internal class Grid
     {
-        bool[,] grid;
-        readonly int width;
-        readonly int height;
-        readonly Dictionary<string, Func<bool[,], Point, Point, bool[,]>> reducers = new Dictionary<string, Func<bool[,], Point, Point, bool[,]>>();
+        private byte[,] grid;
+        private readonly int width;
+        private readonly int height;
+        private readonly Dictionary<string, Func<byte[,], Point, Point, byte[,]>> reducers = new Dictionary<string, Func<byte[,], Point, Point, byte[,]>>();
         public Grid(int width, int height)
         {
             this.width = width;
             this.height = height;
-            grid = new bool[width, height];
+            grid = new byte[width, height];
         }
 
-        public void Register(string ev, Func<bool[,], Point, Point, bool[,]> reducer)
+        public void Register(string ev, Func<byte[,], Point, Point, byte[,]> reducer)
         {
             reducers.Add(ev, reducer);
         }
@@ -64,11 +110,11 @@ namespace Day6
         public Image AsBitmap()
         {
             var img = new Bitmap(width, height);
-            for(var y = 0; y < height; y++)
+            for (var y = 0; y < height; y++)
             {
-                for(var x = 0; x < width; x++)
+                for (var x = 0; x < width; x++)
                 {
-                    img.SetPixel(x, y, grid[x, y] ? Color.White : Color.Black);
+                    img.SetPixel(x, y, grid[x, y] > 0 ? Color.White : Color.Black);
                 }
             }
             return img;
