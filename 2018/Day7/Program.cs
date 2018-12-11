480db40e0198d5c1b7bb2473a8e090271c541b90
John Strand <john.strand@dekiru.se>
2018-12-11 20:45:47 +0100

    2018.07 Began work

---
+++ 2018/Day7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Step
+    {
+        public string Name { get; set; }
+        public bool Resolved { get; set; }
+        public string ResolvedBy { get; set; }
+    }
+}
