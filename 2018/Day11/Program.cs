ef89be01521987c62c25ad330f23d209c4a6cac2
John Strand <john.strand@dekiru.se>
2018-12-11 17:10:28 +0100

    Started project

---
+++ 2018/Day11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Day11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grid = (from x in Enumerable.Range(1, 300)
+                        from y in Enumerable.Range(1, 300)
+                        select new { x, y, p = CalculatePowerLevel(x, y, 9110) }).ToList();
+            Console.Read();
+        }
+
+        static int CalculatePowerLevel(int x, int y, int serialNo)
+        {
+            var rackId = x + 10;
+            var powerLevel = rackId * y;
+            powerLevel += serialNo;
+            powerLevel *= rackId;
+
+            return (powerLevel / 100) % 10 - 5;
+        }
+    }
+}
