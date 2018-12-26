84759cc3ac795a22af0413f3057b6e49f0758643
John Strand <john.strand@dekiru.se>
2018-12-26 13:09:09 +0100

    Day 9 Part 1 doesn't work

--- 2018/Day9/Program.cs
+++ 2018/Day9/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace Day9
@@ -11,11 +12,16 @@ namespace Day9
             var marbles = new List<int> { 0 };
             var active = 0;
             var next = 1;
-            while (next < 26)
+            var players = 10;
+            var player = 0;
+            var limit = 1618;
+            var score = Enumerable.Range(0, players).ToDictionary(x => x, x => 0);
+            while (next < limit + 1)
             {
                 if (next % 23 == 0)
                 {
                     var removed = (active + marbles.Count - 7) % marbles.Count + 1;
+                    score[player] += 23 + marbles[removed];
                     marbles.RemoveAt(removed);
                     active = removed >= marbles.Count ? 0 : removed;
                 }
@@ -25,9 +31,15 @@ namespace Day9
                     marbles.Insert(nextActive + 1, next);
                     active = nextActive;
                 }
-                Console.WriteLine(string.Join(" ", marbles));
-                Thread.Sleep(1000);
+
+                //Console.WriteLine($"[{player + 1}] {string.Join(" ", marbles)}");
+                player = (player + 1) % players;
+                //Thread.Sleep(100);
                 next++;
+            }
+            foreach(var item in score.OrderBy(kv => kv.Key))
+            {
+                Console.WriteLine($"Player {item.Key + 1}: {item.Value}");
             }
             Console.Read();
         }
