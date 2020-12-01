6de1990396dcdf9beb6bbe61dc7304e2fc71d3dc
John Strand <john.strand@dekiru.se>
2020-12-01 16:02:26 +0100

    More brutal

--- 2020/Day1/Program.cs
+++ 2020/Day1/Program.cs
@@ -7,35 +7,41 @@ namespace Day1
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            var buckets = new Dictionary<int, HashSet<int>>();
+            var values = new List<int>();
             foreach (var line in File.ReadAllLines("input.txt"))
             {
-                var bucket = line.Length;
-                if (!buckets.ContainsKey(bucket))
-                {
-                    buckets.Add(bucket, new HashSet<int>());
-                }
-                buckets[bucket].Add(int.Parse(line));
+                values.Add(int.Parse(line));
             }
 
-            foreach (var v1 in buckets[3])
+            var skip = 0;
+
+            var p2 = false;
+
+            foreach (var v1 in values)
             {
-                foreach (var v2 in buckets[4])
+                skip++;
+                foreach (var v2 in values.Skip(skip))
                 {
                     if (v1 + v2 == 2020)
                     {
-                        Console.WriteLine(v1 * v2);
+                        Console.WriteLine($"Part 1: {v1 * v2}");
+                    }
+                    if (p2)
+                    {
+                        continue;
+                    }
+                    foreach (var v3 in values.Skip(skip + 1))
+                    {
+                        if (v1 + v2 + v3 == 2020)
+                        {
+                            Console.WriteLine($"Part 2: {v1 * v2 * v3}");
+                            p2 = true;
+                        }
                     }
                 }
             }
-
-            var values = buckets[3].Union(buckets[4]).ToList();
-
-            var triples = (from v1 in values from v2 in values from v3 in values select new { sum = v1 + v2 + v3, product = v1 * v2 * v3 }).ToList();
-            var triple = triples.First(t => t.sum == 2020);
-            Console.WriteLine(triple.product);
         }
     }
 }
