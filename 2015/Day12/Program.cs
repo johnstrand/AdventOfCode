7d519a6a1b6f7a7c4987ed2067e9ed2b41879c91
John Strand <john.strand@dekiru.se>
2020-12-03 18:51:11 +0100

    2015.12 part 1 & 2

---
+++ 2015/Day12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+var input = JsonSerializer.Deserialize<JsonElement>(File.ReadAllText("input.txt"));
+
+var part1 = Sum(input, false);
+var part2 = Sum(input, true);
+
+Console.WriteLine($"Part 1: {part1}, part 2: {part2}");
+
+int Sum(JsonElement element, bool skipRed)
+{
+    if (element.ValueKind == JsonValueKind.Object)
+    {
+        if (skipRed && element.EnumerateObject().Any(p => p.Value.ValueKind == JsonValueKind.String && p.Value.GetString() == "red"))
+        {
+            return 0;
+        }
+        return element.EnumerateObject().Sum(p => Sum(p.Value, skipRed));
+    }
+    else if (element.ValueKind == JsonValueKind.Array)
+    {
+        return element.EnumerateArray().Sum(e => Sum(e, skipRed));
+    }
+    else if (element.ValueKind == JsonValueKind.Number)
+    {
+        return element.GetInt32();
+    }
+    else
+    {
+        return 0;
+    }
+}
