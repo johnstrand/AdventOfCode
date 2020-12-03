d20743294f77938b60bad696817c1d08578c8e71
John Strand <john.strand@dekiru.se>
2020-12-03 10:16:54 +0100

    2015.10 part 1 & 2

---
+++ 2015/Day10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+var input = new Queue<int>("1113122113".Select(c => c - '0'));
+//var input = new Queue<int>(new[] { 1 });
+for (var iter = 0; iter < 50; iter++)
+{
+    var next = new Queue<int>();
+    while (input.Any())
+    {
+        var digit = input.Dequeue();
+        var count = 1;
+        while (input.Any() && input.Peek() == digit)
+        {
+            input.Dequeue();
+            count++;
+        }
+        next.Enqueue(count);
+        next.Enqueue(digit);
+    }
+    input = next;
+    if (iter == 39 || iter == 49)
+    {
+        Console.WriteLine($"Iteration {iter + 1}: {next.Count}");
+    }
+}
