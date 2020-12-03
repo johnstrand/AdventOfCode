8e0519a1977ef47d403eed2c9636000410f65b83
John Strand <john.strand@dekiru.se>
2020-12-03 09:56:46 +0100

    Top-level programs

--- 2020/Day1/Program.cs
+++ 2020/Day1/Program.cs
@@ -3,44 +3,35 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace Day1
+var values = new List<int>();
+foreach (var line in File.ReadAllLines("input.txt"))
 {
-    internal class Program
+    values.Add(int.Parse(line));
+}
+
+var skip = 0;
+
+var p2 = false;
+
+foreach (var v1 in values)
+{
+    skip++;
+    foreach (var v2 in values.Skip(skip))
     {
-        private static void Main()
+        if (v1 + v2 == 2020)
         {
-            var values = new List<int>();
-            foreach (var line in File.ReadAllLines("input.txt"))
+            Console.WriteLine($"Part 1: {v1 * v2}");
+        }
+        if (p2)
+        {
+            continue;
+        }
+        foreach (var v3 in values.Skip(skip + 1))
+        {
+            if (v1 + v2 + v3 == 2020)
             {
-                values.Add(int.Parse(line));
-            }
-
-            var skip = 0;
-
-            var p2 = false;
-
-            foreach (var v1 in values)
-            {
-                skip++;
-                foreach (var v2 in values.Skip(skip))
-                {
-                    if (v1 + v2 == 2020)
-                    {
-                        Console.WriteLine($"Part 1: {v1 * v2}");
-                    }
-                    if (p2)
-                    {
-                        continue;
-                    }
-                    foreach (var v3 in values.Skip(skip + 1))
-                    {
-                        if (v1 + v2 + v3 == 2020)
-                        {
-                            Console.WriteLine($"Part 2: {v1 * v2 * v3}");
-                            p2 = true;
-                        }
-                    }
-                }
+                Console.WriteLine($"Part 2: {v1 * v2 * v3}");
+                p2 = true;
             }
         }
     }
