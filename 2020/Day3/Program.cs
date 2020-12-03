829b1345eb665eb8a3b2e30a2a3bc377f3b9d0de
John Strand <john.strand@dekiru.se>
2020-12-03 09:54:17 +0100

    2020-03 part 1 & 2

---
+++ 2020/Day3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+var rows = File.ReadAllLines(@"input.txt");
+var steppings = new[]
+{
+    (dx: 1, dy: 1),
+    (dx: 3, dy: 1),
+    (dx: 5, dy: 1),
+    (dx: 7, dy: 1),
+    (dx: 1, dy: 2),
+};
+var w = rows[0].Length;
+var product = 1;
+foreach (var (dx, dy) in steppings)
+{
+    var x = 0;
+    var y = 0;
+    var trees = 0;
+    while ((y += dy) < rows.Length)
+    {
+        x += dx;
+        if (rows[y][x % w] == '#')
+        {
+            trees++;
+        }
+    }
+    product *= trees;
+    Console.WriteLine($"Stepping ({dx}, {dy}) = {trees}");
+}
+
+Console.WriteLine($"Product: {product}");
