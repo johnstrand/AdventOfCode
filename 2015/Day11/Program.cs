14e1c2155af719704dd4a43a1aa6f9513048045f
John Strand <john.strand@dekiru.se>
2020-12-03 10:38:30 +0100

    2015.11, part1 & 2

---
+++ 2015/Day11/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+var password = new List<char> { 'h', 'x', 'b', 'x', 'w', 'x', 'b', 'a' };
+var matches = 0;
+while (matches < 2)
+{
+    var ptr = password.Count - 1;
+    while (true)
+    {
+        password[ptr]++;
+        if (password[ptr] == 'i' || password[ptr] == 'o' || password[ptr] == 'l')
+        {
+            password[ptr]++;
+        }
+
+        if (password[ptr] == '{')
+        {
+            password[ptr] = 'a';
+            ptr--;
+            if (ptr == -1)
+            {
+                password.Insert(0, 'a');
+                break;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+    if (IsValid(password))
+    {
+        Console.WriteLine(string.Join("", password));
+        matches++;
+    }
+}
+
+static bool IsValid(List<char> password)
+{
+    var pairs = new HashSet<char>();
+    var incr = false;
+    for (var i = 0; i < password.Count; i++)
+    {
+        if (i > 0 && password[i] == password[i - 1])
+        {
+            pairs.Add(password[i]);
+        }
+        if (i > 1)
+        {
+            var c1 = password[i - 2] + 2;
+            var c2 = password[i - 1] + 1;
+            var c3 = password[i];
+            incr = incr || (c1 == c2 && c1 == c3);
+        }
+    }
+
+    return pairs.Count > 1 && incr;
+}
