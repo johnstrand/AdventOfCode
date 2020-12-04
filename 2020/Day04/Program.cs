6d3c55e8ac0802ffa8fea1cbe2c10387b24b5440
John Strand <john.strand@dekiru.se>
2020-12-04 08:30:24 +0100

    2020.04 part 1

---
+++ 2020/Day04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var data = new Queue<string>(File.ReadLines("input.txt"));
+var passport = new Dictionary<string, string>();
+var part1 = 0;
+while (data.Count > 0)
+{
+    var next = data.Dequeue();
+    if (next == string.Empty)
+    {
+        if (passport.Count == 8 || (passport.Count == 7 && !passport.ContainsKey("cid")))
+        {
+            part1++;
+        }
+        passport.Clear();
+    }
+    else
+    {
+        next.Split(' ').Select(item => item.Split(':')).ToList().ForEach(pair => passport.Add(pair[0], pair[1]));
+    }
+}
+if (passport.Count == 8 || (passport.Count == 7 && !passport.ContainsKey("cid")))
+{
+    part1++;
+}
+
+Console.WriteLine($"Part 1: {part1}");
