ff6918211cdf3f5f490443b3760b1d81117bf3fc
John Strand <john.strand@dekiru.se>
2020-12-06 21:58:12 +0100

    Bit o cleaning

--- 2018/Day1/Program.cs
+++ 2018/Day1/Program.cs
@@ -3,35 +3,25 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace Day1
+var seen = new HashSet<int>();
+var seed = 0;
+var running = true;
+var first = true;
+while (running)
 {
-    class Program
+    seed = File.ReadAllLines("input.txt").Select(int.Parse).Aggregate(seed, (acc, cur) =>
     {
-        static void Main(string[] args)
+        if (seen.Contains(acc) && running)
         {
-            var seen = new HashSet<int>();
-            var seed = 0;
-            var running = true;
-            var first = true;
-            while (running)
-            {
-                seed = File.ReadAllLines("Input.txt").Select(int.Parse).Aggregate(seed, (acc, cur) =>
-                {
-                    if (seen.Contains(acc) && running)
-                    {
-                        Console.WriteLine(acc);
-                        running = false;
-                    }
-                    seen.Add(acc);
-                    return acc + cur;
-                });
-                if (first)
-                {
-                    Console.WriteLine(seed);
-                    first = false;
-                }
-            }
-            Console.Read();
+            Console.WriteLine(acc);
+            running = false;
         }
+        seen.Add(acc);
+        return acc + cur;
+    });
+    if (first)
+    {
+        Console.WriteLine(seed);
+        first = false;
     }
 }
--- 2018/Day2/Program.cs
+++ 2018/Day2/Program.cs
@@ -3,41 +3,30 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace Day2
+var two = 0;
+var three = 0;
+var candidates = new List<string>();
+foreach (var row in File.ReadAllLines("input.txt"))
 {
-    internal class Program
+    var g = row.GroupBy(c => c);
+    if (g.Any(x => x.Count() == 2))
     {
-        private static void Main(string[] args)
-        {
-            var two = 0;
-            var three = 0;
-            var candidates = new List<string>();
-            foreach (var row in File.ReadAllLines("Input.txt"))
-            {
-                var g = row.GroupBy(c => c);
-                if (g.Any(x => x.Count() == 2))
-                {
-                    two++;
-                }
-                if (g.Any(x => x.Count() == 3))
-                {
-                    three++;
-                }
-                foreach (var candidate in candidates)
-                {
-                    var filtered = row.Zip(candidate, (c1, c2) => c1 == c2 ? c1 : ' ').Where(c => c != ' ');
-                    if (filtered.Count() == row.Length - 1)
-                    {
-                        Console.WriteLine($"{row} :: {candidate}");
-                        Console.WriteLine(string.Join("", filtered));
-                    }
-                }
-                candidates.Add(row);
-            }
-
-            Console.WriteLine($"{two} * {three} = {two * three}");
-            Console.ReadLine();
-        }
-
+        two++;
     }
+    if (g.Any(x => x.Count() == 3))
+    {
+        three++;
+    }
+    foreach (var candidate in candidates)
+    {
+        var filtered = row.Zip(candidate, (c1, c2) => c1 == c2 ? c1 : ' ').Where(c => c != ' ');
+        if (filtered.Count() == row.Length - 1)
+        {
+            Console.WriteLine($"{row} :: {candidate}");
+            Console.WriteLine(string.Join("", filtered));
+        }
+    }
+    candidates.Add(row);
 }
+
+Console.WriteLine($"{two} * {three} = {two * three}");
