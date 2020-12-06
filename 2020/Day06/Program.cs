0e617914ec70605c59516b9ca51c0ba21c64bdbd
John Strand <john.strand@dekiru.se>
2020-12-06 08:52:43 +0100

    2020.06, part 1 & 2

---
+++ 2020/Day06/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var currentGroup = new Dictionary<char, int>();
+var part1 = 0;
+var part2 = 0;
+var groupCount = 0;
+foreach (var row in File.ReadAllLines("input.txt"))
+{
+    if (row == string.Empty)
+    {
+        part1 += currentGroup.Count;
+        part2 += currentGroup.Values.Count(v => v == groupCount);
+        groupCount = 0;
+        currentGroup.Clear();
+    }
+    else
+    {
+        groupCount++;
+        foreach (var c in row)
+        {
+            if (currentGroup.ContainsKey(c))
+            {
+                currentGroup[c]++;
+            }
+            else
+            {
+                currentGroup.Add(c, 1);
+            }
+        }
+    }
+}
+
+Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part2}");
