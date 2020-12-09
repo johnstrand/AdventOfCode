67f0c288c4369537e9d49ce164a9fe8e817de48f
John Strand <john.strand@dekiru.se>
2020-12-09 20:53:43 +0100

    2016.01 part 2 actually working

--- 2016/Day1/Program.cs
+++ 2016/Day1/Program.cs
@@ -4,44 +4,39 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 
-namespace Day1
+var pos = new Point(0, 0);
+var facing = 0;
+var seen = new HashSet<Point>();
+var first = true;
+foreach (var command in File.ReadAllText("input.txt").Split(',').Select(c => c.Trim()))
 {
-    internal class Program
+    if (command[0] == 'R')
     {
-        private static void Main(string[] args)
+        facing = (facing + 1) % 4;
+    }
+    else
+    {
+        facing--;
+        if (facing == -1)
         {
-            var pos = new Point(0, 0);
-            var facing = 0;
-            var seen = new HashSet<Point>();
-            foreach (var command in File.ReadAllText("Input.txt").Split(',').Select(c => c.Trim()))
-            {
-                if (command[0] == 'R')
-                {
-                    facing = (facing + 1) % 4;
-                }
-                else
-                {
-                    facing--;
-                    if (facing == -1)
-                    {
-                        facing = 3;
-                    }
-                }
+            facing = 3;
+        }
+    }
 
-                var dist = int.Parse(command.Substring(1));
+    var dist = int.Parse(command.Substring(1));
+    for (var step = 0; step < dist; step++)
+    {
+        pos.Offset(
+            facing == 1 ? 1 : facing == 3 ? -1 : 0,
+            facing == 0 ? -1 : facing == 2 ? 1 : 0);
 
-                pos.Offset(
-                    facing == 1 ? dist : facing == 3 ? -dist : 0,
-                    facing == 0 ? -dist : facing == 2 ? dist : 0);
-
-                if (!seen.Add(pos))
-                {
-                    Console.WriteLine($"Seen twice: {Math.Abs(pos.X) + Math.Abs(pos.Y)}");
-                }
-            }
-
-            Console.WriteLine($"Distance: {Math.Abs(pos.X) + Math.Abs(pos.Y)}");
-            Console.Read();
+        if (first && !seen.Add(pos))
+        {
+            Console.WriteLine($"Seen twice: {Math.Abs(pos.X) + Math.Abs(pos.Y)}");
+            first = false;
         }
     }
 }
+
+Console.WriteLine($"Distance: {Math.Abs(pos.X) + Math.Abs(pos.Y)}");
+Console.Read();
