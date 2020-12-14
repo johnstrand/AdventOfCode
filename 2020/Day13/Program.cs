0f25bf89e0fadb69c11494d6c4b7d084847dbfcc
John Strand <john.strand@dekiru.se>
2020-12-14 19:20:03 +0100

    2020.13, part 1 & 2

---
+++ 2020/Day13/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+//var input = new[] { "939", "7,13,x,x,59,x,31,19" };
+
+var input = new[] { "1007268", "17,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,937,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,x,23,x,x,x,x,x,29,x,397,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,19" };
+
+var currentTime = int.Parse(input.First());
+
+var times = input.Last().Split(',').Where(IsNumber).Select(int.Parse).ToList();
+
+var ts = currentTime;
+while (true)
+{
+    var num = times.FirstOrDefault(t => ts % t == 0);
+    if (num > 0)
+    {
+        Console.WriteLine($"Part 1: {num * (ts - currentTime)}");
+        break;
+    }
+    ts++;
+}
+
+var t = 0L;
+var rules = input.Last().Split(',').Select((n, i) => n == "x" ? (-1, -1) : (value: long.Parse(n), offset: i)).Where(x => x.value != -1).ToList();
+var missing = 1;
+var multiplier = rules.First().value;
+
+while (missing < rules.Count)
+{
+    t += multiplier;
+    var next = rules[missing];
+    if ((t + next.offset) % next.value == 0)
+    {
+        multiplier *= next.value;
+        missing++;
+    }
+}
+
+Console.WriteLine($"Part 2: {t}");
+
+var largest = rules.First(r => r.value == rules.Max(r => r.value));
+
+var s = 0L;
+while (true)
+{
+    s += largest.value;
+    if (s + largest.offset == 626670513163231)
+    {
+    }
+}
+
+bool IsNumber(string str)
+{
+    return str.All(char.IsDigit);
+}
