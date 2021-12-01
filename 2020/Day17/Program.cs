569c646832a2ae275f35a4e6ce45a833d1feb176
John Strand <john.strand@dekiru.se>
2021-12-01 16:15:15 +0100

    Part 1

--- 2020/Day17/Program.cs
+++ 2020/Day17/Program.cs
@@ -1,62 +1,134 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-var grid = new Grid().Load(3, 3, ".#...####");
+var grid = new Grid(File.ReadAllLines("input.txt"));
 
-Console.WriteLine(grid.GetValue(1, 0, 0));
-
-class Grid
+for (var cycle = 1; cycle <= 6; cycle++)
 {
-    private readonly Dictionary<Point, char> points = new Dictionary<Point, char>();
+    Console.WriteLine($"Cycle: {cycle}");
+    grid.Display();
+    grid.Step();
+    Console.WriteLine();
+}
+Console.WriteLine($"Active: {grid.Active}");
 
-    public IEnumerable<char> GetNeighbors(int x, int y, int z)
+internal class Grid
+{
+    private HashSet<(int x, int y, int z)> points = new();
+
+    public int Active => points.Count;
+
+    private bool IsActive(int x, int y, int z)
     {
-        foreach (var tz in new[] { z - 1, z, z + 1 })
+        return points.Contains((x, y, z));
+    }
+
+    private int Adjacent(int x, int y, int z)
+    {
+        return (
+            from tz in Enumerable.Range(z - 1, 3)
+            from ty in Enumerable.Range(y - 1, 3)
+            from tx in Enumerable.Range(x - 1, 3)
+            where !(tx == x && ty == y && tz == z) && IsActive(tx, ty, tz)
+            select 1).Sum();
+    }
+
+    public void Display()
+    {
+        GetMinMax(out int minZ, out int minY, out int minX, out int maxZ, out int maxY, out int maxX);
+        for (var z = minZ; z <= maxZ; z++)
         {
-            foreach (var ty in new[] { y - 1, y, y + 1 })
+            Console.WriteLine($"z={z}");
+            for (var y = minY; y <= maxY; y++)
             {
-                foreach (var tx in new[] { x - 1, x, x + 1 })
+                for (int x = minX; x <= maxX; x++)
                 {
-                    if (tz == z && ty == y && tx == x)
+                    var active = IsActive(x, y, z);
+                    Console.Write(active ? "#" : " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public void Step()
+    {
+        var newPoints = new HashSet<(int x, int y, int z)>();
+        GetMinMax(out int minZ, out int minY, out int minX, out int maxZ, out int maxY, out int maxX);
+        for (var z = minZ - 1; z <= maxZ + 1; z++)
+        {
+            for (var y = minY - 1; y <= maxY + 1; y++)
+            {
+                for (int x = minX - 1; x <= maxX + 1; x++)
+                {
+                    var active = IsActive(x, y, z);
+                    var adj = Adjacent(x, y, z);
+                    //Console.WriteLine($"({x}, {y}, {z}) = {active}/{adj}");
+                    var nextActive = (active && adj is 2 or 3) || (!active && adj == 3);
+                    if (nextActive)
                     {
-                        continue;
+                        newPoints.Add((x, y, z));
                     }
-                    yield return GetValue(x, y, z);
                 }
             }
         }
+
+        points = newPoints;
     }
 
-    public Grid Load(int w, int h, string source)
+    private void GetMinMax(out int minZ, out int minY, out int minX, out int maxZ, out int maxY, out int maxX)
     {
-        for (var y = 0; y < h; y++)
+        minZ = minY = minX = int.MaxValue;
+        maxZ = maxY = maxX = int.MinValue;
+        foreach (var (x, y, z) in points)
         {
-            for (var x = 0; x < w; x++)
+            if (x < minX)
             {
-                SetValue(x, y, 0, source[y * w + x] == '#');
+                minX = x;
+            }
+
+            if (y < minY)
+            {
+                minY = y;
+            }
+
+            if (z < minZ)
+            {
+                minZ = z;
+            }
+
+            if (x > maxX)
+            {
+                maxX = x;
+            }
+
+            if (y > maxY)
+            {
+                maxY = y;
+            }
+
+            if (z > maxZ)
+            {
+                maxZ = z;
             }
         }
-
-        return this;
     }
 
-    public char GetValue(int x, int y, int z)
+    public Grid(string[] baseState)
     {
-        var point = new Point(x, y, z);
-        if (!points.ContainsKey(point))
+        for (var y = 0; y < baseState.Length; y++)
         {
-            points[point] = '.';
+            for (var x = 0; x < baseState[y].Length; x++)
+            {
+                if (baseState[y][x] != '#')
+                {
+                    continue;
+                }
+                points.Add((x, y, 0));
+            }
         }
-
-        return points[point];
-    }
-
-    public Grid SetValue(int x, int y, int z, bool set)
-    {
-        var point = new Point(x, y, z);
-        points[point] = set ? '#' : '.';
-        return this;
     }
 }
-
-record Point(int X, int Y, int Z);
