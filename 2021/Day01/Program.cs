f71e2a70ddd8ddff450c66db5a5ce235e4911df8
John Strand <john.strand@dekiru.se>
2021-12-01 09:37:09 +0100

    2021 part 1 & 2

---
+++ 2021/Day01/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+var values = File.ReadAllLines("input.txt").Select(int.Parse).ToList();
+var current = values[0];
+var increase = 0;
+foreach (var value in values.Skip(1))
+{
+    if (value > current)
+    {
+        increase++;
+    }
+
+    current = value;
+}
+
+Console.WriteLine($"Part 1: {increase}.");
+
+current = -1;
+increase = 0;
+
+for (var i = 0; i < values.Count - 2; i++)
+{
+    var window = values.GetRange(i, 3).Sum();
+    if (current == -1)
+    {
+        current = window;
+        continue;
+    }
+    if (window > current)
+    {
+        increase++;
+    }
+    current = window;
+}
+
+Console.WriteLine($"Part 2: {increase}.");
