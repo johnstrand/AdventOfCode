5a1cb5bb6239aabcae78db0a8a2510b49a52c126
John Strand <john.strand@dekiru.se>
2021-12-06 10:18:32 +0100

    2021, day 6, part 1, also WIP

--- 2021/Day06/Program.cs
+++ 2021/Day06/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var fishList = new List<List<int>>
+{
+    File.ReadAllText("input-test.txt").Split(',').Select(int.Parse).ToList()
+};
+
+var prev = 0;
+for (var day = 0; day < 256; day++)
+{
+    var limit = fishList.Count;
+    for (var schoolIndex = 0; schoolIndex < fishList.Count; schoolIndex++)
+    {
+        var fish = fishList[schoolIndex];
+        var newSpawn = 0;
+        for (var index = 0; index < fish.Count; index++)
+        {
+            fish[index]--;
+            if (fish[index] == -1)
+            {
+                fish[index] = 6;
+                newSpawn++;
+            }
+        }
+        if (newSpawn > 0)
+        {
+            var newFish = Enumerable.Repeat(8, newSpawn).ToList();
+            if (schoolIndex + 1 == fishList.Count)
+            {
+                fishList.Add(newFish);
+            }
+            else
+            {
+                fishList[schoolIndex + 1].AddRange(newFish);
+            }
+        }
+    }
+
+    var current = fishList.Sum(school => school.Count);
+    Console.WriteLine($"Day {day + 1}: {current} (+{current - prev})");
+    prev = current;
+}
+
+//Console.WriteLine(string.Join(", ", fish));
