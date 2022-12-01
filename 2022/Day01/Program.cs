21b4b9e190f710c90ac7b7c53ba90d20f2094e86
John Strand <johdeg@gmail.com>
2022-12-01 09:13:39 +0100

    2022.1 parts 1 & 2

---
+++ 2022/Day01/Program.cs
@@ -0,0 +1,20 @@
+﻿var calorieCounts = File.ReadAllLines("input.txt").Aggregate(new List<int> { 0 }, (acc, cur) =>
+{
+    if (string.IsNullOrWhiteSpace(cur))
+    {
+        acc.Add(0);
+        return acc;
+    }
+
+    acc[^1] += int.Parse(cur);
+
+    return acc;
+});
+
+var max = calorieCounts.Max();
+
+Console.WriteLine($"Part 1: {max}");
+
+var top3 = calorieCounts.OrderByDescending(c => c).Take(3).Sum();
+
+Console.WriteLine($"Part 2: {top3}");
