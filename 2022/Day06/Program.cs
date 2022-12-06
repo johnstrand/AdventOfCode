a0f1eb4d6225d99cf39b00371bc0ff9315c74a83
John Strand <johdeg@gmail.com>
2022-12-06 14:45:02 +0100

    2022.6, part 1 & 2

---
+++ 2022/Day06/Program.cs
@@ -0,0 +1,21 @@
+﻿foreach (var seq in File.ReadLines("input.txt"))
+{
+    var p1 = false;
+    var p2 = false;
+    for (var i = 4; i < seq.Length && !(p1 && p2); i++)
+    {
+        // Console.WriteLine($"Testing {seq.Substring(i - 4, 4)}");
+
+        if (!p2 && i > 14 && seq.Substring(i - 14, 14).Distinct().Count() == 14)
+        {
+            Console.WriteLine($"Part 2: {i}");
+            p2 = true;
+        }
+
+        if (!p1 && seq.Substring(i - 4, 4).Distinct().Count() == 4)
+        {
+            Console.WriteLine($"Part 1: {i}");
+            p1 = true;
+        }
+    }
+}
