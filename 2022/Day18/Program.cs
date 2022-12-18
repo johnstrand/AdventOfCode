a15aecd3faf79e7b1aec5ad050ebd404ffde1814
John Strand <johdeg@gmail.com>
2022-12-18 20:33:32 +0100

    2022.18, part 1 & 2

---
+++ 2022/Day18/Program.cs
@@ -0,0 +1,126 @@
+﻿using AoC.Common;
+
+var nodes = new HashSet<Point3>();
+foreach (var row in File.ReadAllLines("input.txt"))
+{
+    var coords = row.Split(",").Select(int.Parse).ToList();
+    nodes.Add(new(coords[0], coords[1], coords[2]));
+}
+
+var visited = new HashSet<Point3>();
+
+var deltas = new[]
+{
+    new Point3(x: 1, y: 0, z: 0),
+    new(x: -1, y: 0, z: 0),
+    new(x: 0, y: 1, z: 0),
+    new(x: 0, y: -1, z: 0),
+    new(x: 0, y: 0, z: 1),
+    new(x: 0, y: 0, z: -1),
+};
+
+var faces = 0;
+
+void Visit(Point3 node, int depth = 0)
+{
+    if (!visited!.Add(node))
+    {
+        return;
+    }
+
+    // Console.WriteLine($"{new string(' ', depth)}Visiting {node}");
+
+    foreach (var adjacent in deltas.Select(d => d + node))
+    {
+        if (!nodes!.Contains(adjacent))
+        {
+            faces++;
+        }
+        else
+        {
+            Visit(adjacent, depth + 1);
+        }
+    }
+}
+
+while (visited.Count != nodes.Count)
+{
+    Visit(nodes.First(n => !visited.Contains(n)));
+}
+
+Console.WriteLine($"Part 1: {faces}");
+
+var minX = nodes.Min(n => n.X);
+var minY = nodes.Min(n => n.Y);
+var minZ = nodes.Min(n => n.Z);
+
+var maxX = nodes.Max(n => n.X);
+var maxY = nodes.Max(n => n.Y);
+var maxZ = nodes.Max(n => n.Z);
+
+for (var z = minZ; z <= maxZ; z++)
+{
+    for (var y = minY; y <= maxY; y++)
+    {
+        for (var x = minX; x <= maxX; x++)
+        {
+            var candidate = new Point3(x, y, z);
+
+            if (nodes.Contains(candidate))
+            {
+                continue;
+            }
+
+            var temp = new HashSet<Point3>() { candidate };
+            var pending = new Queue<Point3>();
+
+            pending.Enqueue(candidate);
+
+            while (pending.Count > 0)
+            {
+                var point = pending.Dequeue();
+
+                if (nodes.Contains(point))
+                {
+                    continue;
+                }
+
+                foreach (var adjacent in deltas.Select(d => d + point))
+                {
+                    if (nodes.Contains(adjacent) || !temp.Add(adjacent))
+                    {
+                        continue;
+                    }
+
+                    if (adjacent.X < minX || adjacent.Y < minY || adjacent.Z < minZ || adjacent.X > maxX || adjacent.Y > maxY || adjacent.Z > maxZ)
+                    {
+                        temp.Clear();
+                        break;
+                    }
+
+                    pending.Enqueue(adjacent);
+                }
+
+                if (temp.Count == 0)
+                {
+                    break;
+                }
+            }
+
+            foreach (var n in temp)
+            {
+                nodes.Add(n);
+            }
+        }
+    }
+}
+
+visited.Clear();
+faces = 0;
+
+while (visited.Count != nodes.Count)
+{
+    Visit(nodes.First(n => !visited.Contains(n)));
+}
+
+Console.WriteLine($"Part 2: {faces}");
---
+++ AoC.Common/Point3.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace AoC.Common;
+
+public class Point3
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Z { get; set; }
+
+    public static Point3 Origin { get; } = new(0, 0, 0);
+
+    public Point3(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public void Normalize()
+    {
+        X = Normalize(X);
+        Y = Normalize(Y);
+        Z = Normalize(Z);
+    }
+
+    private static int Normalize(int value)
+    {
+        return value == 0 ? 0 : value / Math.Abs(value);
+    }
+
+    public static bool operator ==(Point3? p1, Point3? p2)
+    {
+        return p1 is null || p2 is null ? p1 is null && p2 is null : p1.X == p2.X && p1.Y == p2.Y && p1.Z == p2.Z;
+    }
+
+    public static bool operator !=(Point3? p1, Point3? p2)
+    {
+        return !(p1 == p2);
+    }
+
+    public static Point3 operator +(Point3 p1, Point3 p2)
+    {
+        return new(p1.X + p2.X, p1.Y + p2.Y, p1.Z + p2.Z);
+    }
+
+    public static Point3 operator -(Point3 p1, Point3 p2)
+    {
+        return new(p1.X - p2.X, p1.Y - p2.Y, p1.Z + p2.Z);
+    }
+
+    public override bool Equals([NotNullWhen(true)] object? obj)
+    {
+        return obj is Point3 p && p == this;
+    }
+
+    public override int GetHashCode()
+    {
+        return unchecked(X ^ Y ^ Z);
+    }
+
+    public override string ToString()
+    {
+        return $"{X}x{Y}x{Z}";
+    }
+}
