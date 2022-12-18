a57cbe10bfe2a115bece8bafbd359001225b59fd
John Strand <johdeg@gmail.com>
2022-12-18 15:22:53 +0100

    2022.16, part 1

--- 2022/Day16/Program.cs
+++ 2022/Day16/Program.cs
@@ -25,22 +25,18 @@ foreach (var node in valves.Values)
 
 var candidateValues = valves.Values.Where(v => v.Flow > 0).Select(v => v.Id).ToList();
 
-var variants = Fact(candidateValues.Count);
+var paths = new Queue<List<string>>(candidateValues.Select(c => new List<string> { c }));
+
 var maxP = 0;
-var sync = new object();
-var progress = 0L;
 
-var flowMax = new Dictionary<string, int>();
-
-foreach (var path in Permutations.Generate(candidateValues))
+while (paths.Count > 0)
 {
-    progress++;
-    Console.Write($"{progress:N0} / {variants:N0}\r");
+    Console.Write($"Max: {maxP}. Pending: {paths.Count:N0}     \r");
+    var path = paths.Dequeue();
     var currentNode = "AA";
     var totalTime = 0;
     var totalP = 0;
 
-    //foreach (var node in path)
     for (var i = 0; i < path.Count; i++)
     {
         var node = path[i];
@@ -53,29 +49,25 @@ foreach (var path in Permutations.Generate(candidateValues))
         }
 
         totalP += (30 - totalTime) * valves[node].Flow;
-        if (flowMax.TryGetValue(node, out var m) && m > totalP)
-        {
-            break;
-        }
-
-        flowMax[node] = totalP;
         currentNode = node;
+    }
+
+    if (totalTime < 30)
+    {
+        foreach (var remaining in candidateValues.Where(c => !path.Contains(c)))
+        {
+            paths.Enqueue(path.Append(remaining).ToList());
+        }
+        continue;
     }
 
     if (totalP > maxP)
     {
         maxP = totalP;
-        Console.WriteLine();
-        Console.WriteLine($"Total: {maxP}");
     }
-
 }
 
+Console.Write($"Max: {maxP}. Pending: {paths.Count:N0}     \r");
 Console.WriteLine();
-
-static long Fact(long v)
-{
-    return v < 2 ? 1 : v * Fact(v - 1);
-}
 
 internal record Valve(string Id, int Flow, List<string> Connections, bool Open = false);
