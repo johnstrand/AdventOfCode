4cec353ddb3137b00596ac93aed826e83fc3ace5
John Strand <john.strand@dekiru.se>
2023-12-04 16:58:19 +0100

    2016.09, part 1

---
+++ 2016/Day09/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+foreach (var line in File.ReadLines("input-test.txt"))
+{
+    var sb = new StringBuilder();
+
+    Console.Write(line + " -> ");
+    for (var i = 0; i < line.Length; i++)
+    {
+        if (line[i] == '(')
+        {
+            var length = 0;
+            var repeat = 0;
+            while (char.IsNumber(line[++i]))
+            {
+                length = (length * 10) + (line[i] - '0');
+            }
+
+            while (char.IsNumber(line[++i]))
+            {
+                repeat = (repeat * 10) + (line[i] - '0');
+            }
+
+            i++;
+
+            var substring = line.Substring(i, length);
+            // i += length - 1;
+            sb.Append(string.Concat(Enumerable.Repeat(substring, repeat)));
+        }
+        else
+        {
+            sb.Append(line[i]);
+        }
+    }
+    var decompressed = sb.ToString();
+
+    Console.WriteLine(decompressed.Length);
+    //Console.WriteLine($"{decompressed} ({decompressed.Length})");
+}
