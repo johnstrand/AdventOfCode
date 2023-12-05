3b57da53674ac6f2da8a30dae7696c8efd2c1784
John Strand <john.strand@dekiru.se>
2023-12-05 11:46:35 +0100

    2023.05, part 1 & 2

--- 2023/Day05/Program.cs
+++ 2023/Day05/Program.cs
@@ -2,7 +2,7 @@
 
 var seeds = new List<long>();
 
-var maps = new Dictionary<string, Dictionary<long, long>>
+var maps = new Dictionary<string, HashSet<Range>>
 {
     ["seed"] = [],
     ["soil"] = [],
@@ -24,11 +24,57 @@ var sequence = new[]
     "humidity",
 };
 
+var reverseSequence = new[]
+{
+    "humidity",
+    "temperature",
+    "light",
+    "water",
+    "fertilizer",
+    "soil",
+    "seed",
+};
+
 var currentMap = "";
 
 long MapValue(string map, long value)
 {
-    return maps![map].TryGetValue(value, out var result) ? result : value;
+    var ranges = maps[map];
+
+    var range = ranges.FirstOrDefault(r => r.Source <= value && value < r.Source + r.Length);
+
+    if (range != null)
+    {
+        var offset = value - range.Source;
+        return range.Destination + offset;
+    }
+
+    return value;
+}
+
+long ReverseMapValue(string map, long value)
+{
+    var ranges = maps![map];
+
+    var range = ranges.FirstOrDefault(r => r.Destination <= value && value < r.Destination + r.Length);
+
+    if (range != null)
+    {
+        var offset = value - range.Destination;
+        return range.Source + offset;
+    }
+
+    return value;
+}
+
+long ReverseMapSequence(long value)
+{
+    foreach (var map in reverseSequence!)
+    {
+        value = ReverseMapValue(map, value);
+    }
+
+    return value;
 }
 
 Console.WriteLine("Reading input...");
@@ -54,14 +100,11 @@ foreach (var line in File.ReadAllLines("input.txt"))
     }
 
     var numbers = line.ToNumbers().ToList();
-    var destination = numbers[1];
-    var source = numbers[0];
+    var destination = numbers[0];
+    var source = numbers[1];
     var length = numbers[2];
 
-    for (var i = 0; i < length; i++)
-    {
-        maps[currentMap][destination + i] = source + i;
-    }
+    maps[currentMap].Add(new Range(source, destination, length));
 }
 
 var min = long.MaxValue;
@@ -79,3 +122,18 @@ foreach (var seed in seeds)
 }
 
 Render.Result("Part 1", min);
+
+var seedRanges = Enumerable.Range(0, seeds.Count / 2).Select(i => (from: seeds[i * 2], to: seeds[i * 2] + seeds[(i * 2) + 1] - 1)).ToList();
+
+var i = 0;
+while (true)
+{
+    var value = ReverseMapSequence(++i);
+    if (seedRanges.Any(r => r.from <= value && value <= r.to))
+    {
+        Render.Result("Part 2:", i);
+        break;
+    }
+}
+
+internal record Range(long Source, long Destination, long Length);
