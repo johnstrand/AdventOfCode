0fe1bb47ccfcc5cde9fc08297239abf65bab816a
John Strand <john.strand@dekiru.se>
2023-12-05 10:21:56 +0100

    2016.09, part 2

--- 2016/Day09/Program.cs
+++ 2016/Day09/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System.Text;
 
-foreach (var line in File.ReadLines("input-test.txt"))
+using AoC.Common;
+
+foreach (var line in File.ReadLines("input.txt"))
 {
     var sb = new StringBuilder();
 
-    Console.Write(line + " -> ");
+    // Console.Write(line + " -> ");
     for (var i = 0; i < line.Length; i++)
     {
         if (line[i] == '(')
@@ -24,7 +26,7 @@ foreach (var line in File.ReadLines("input-test.txt"))
             i++;
 
             var substring = line.Substring(i, length);
-            // i += length - 1;
+            i += length - 1;
             sb.Append(string.Concat(Enumerable.Repeat(substring, repeat)));
         }
         else
@@ -35,5 +37,34 @@ foreach (var line in File.ReadLines("input-test.txt"))
     var decompressed = sb.ToString();
 
     Console.WriteLine(decompressed.Length);
-    //Console.WriteLine($"{decompressed} ({decompressed.Length})");
+}
+
+static long Expand(string input, int depth = 0)
+{
+    var length = (long)input.Length;
+    var searchStart = -1;
+    while ((searchStart = input.IndexOf('(', searchStart + 1)) != -1)
+    {
+        var end = input.IndexOf(')', searchStart);
+        var marker = input.Substring(searchStart + 1, end - searchStart - 1).ToNumbers('x').ToArray();
+        var markerLength = end - searchStart + 1;
+        // Console.WriteLine($"{new string(' ', depth)}{input} -> {length} -= {markerLength} + {marker[0]}");
+        length -= markerLength + marker[0];
+        var markerContent = input.Substring(end + 1, (int)marker[0]);
+        searchStart += markerLength + (int)marker[0] - 1;
+
+        var next = Expand(markerContent, depth + 1);
+        // Console.WriteLine($"{new string(' ', depth)}{length} += {marker[1]} * {next}");
+        length += marker[1] * next;
+    }
+
+    // Console.WriteLine($"{new string(' ', depth)}{input} -> {length}");
+
+    return length;
+}
+
+foreach (var line in File.ReadLines("input.txt"))
+{
+    // Console.WriteLine($"{line} -> {Expand(line)}");
+    Console.WriteLine(Expand(line));
 }
