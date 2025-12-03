46a39c926fc16beac296e149fa05bad4ccceac64
John Strand <john.strand@dekiru.se>
2025-12-03 16:56:48 +0100

    2025.03, part 1

---
+++ 2025/Day03/Program.cs
@@ -0,0 +1,24 @@
+﻿using AoC.Common;
+
+var part1 = 0;
+foreach (var bank in Input.ReadTest())
+{
+    var max = 0;
+    for (var i = 0; i < bank.Length; i++)
+    {
+        for (var j = i + 1; j < bank.Length; j++)
+        {
+            var value = (bank[i] - '0') * 10 + (bank[j] - '0');
+
+            if (value > max)
+            {
+                max = value;
+            }
+        }
+    }
+    Console.WriteLine($"Bank: {bank[0..10]} => Max pair value: {max}");
+
+    part1 += max;
+}
+
+Console.WriteLine($"Part 1: {part1}");
